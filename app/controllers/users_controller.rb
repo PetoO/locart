c50040d351bd2657100f36aec28696a2d6c04767
PetoO <zapalac.peter@gmail.com>
2015-12-07 00:25:42 +0100

    asdasd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	def show
 	  @user = User.find(params[:id])
-	  @locations = Location.all.order("created_at DESC").where(user_id: params[:id]).order("created_at DESC").paginate(:page => params[:page],:per_page => 16)
+	  @locations = Location.all.order("created_at DESC").where(user_id: params[:id]).order("created_at DESC").paginate(:page => params[:page],:per_page => 8)
 	  @liked_locations = @user.find_liked_items[0...9]
 	  
 	  @locations_added= @locations.size
